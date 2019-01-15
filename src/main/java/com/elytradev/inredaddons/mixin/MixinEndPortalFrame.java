58d1d4c92f8b26c436f36d5e02e91de3841033b7
Meredith Espinosa <goclonefilms@gmail.com>
2019-01-15 00:58:32 -0800

    actually register the mixins for cake and end portal frame whoops

--- src/main/java/com/elytradev/inredaddons/mixin/MixinEndPortalFrame.java
+++ src/main/java/com/elytradev/inredaddons/mixin/MixinEndPortalFrame.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(EndPortalFrameBlock.class)
-public class MixinEndPortalBlock implements SimpleEncoderScannable {
+public class MixinEndPortalFrame implements SimpleEncoderScannable {
 	@Shadow @Final public static BooleanProperty field_10958;
 
 	@Override
